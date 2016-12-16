7cbf76341cc74c719510fa392f28efe871509bc2
No Name <dfnoname@users.noreply.github.com>
2016-12-16 22:17:17 +0700

    Update dataku.php

--- dataku.php
+++ dataku.php
@@ -1,10 +1,10 @@
 <?php
 
-echo 'inode= '.print_r(fileinode("index.php"));
-echo '======================<br>';
+echo ' inodes '.print_r(fileinode("index.php"));
+echo '<br>======================<br>';
 $bytes = disk_free_space("."); 
     $si_prefix = array( 'B', 'KB', 'MB', 'GB', 'TB', 'EB', 'ZB', 'YB' );
     $base = 1024;
     $class = min((int)log($bytes , $base) , count($si_prefix) - 1);
-    echo $bytes . '<br />';
-    echo sprintf('%1.2f' , $bytes / pow($base,$class)) . ' ' . $si_prefix[$class] . '<br />';
+    //echo $bytes . '<br />';
+    echo "Storage ".sprintf('%1.2f' , $bytes / pow($base,$class)) . ' ' . $si_prefix[$class] . '<br />';
